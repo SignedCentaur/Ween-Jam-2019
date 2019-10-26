9610f6d3c3a24db799c4de4f20e55ca203a9e3da
JackHill-SJ <hilljack1856@gmail.com>
2019-10-26 01:30:47 -0400

    Added Player, Enemy follow, and a basic whitebox of house
    Have to get the nav mesh working with the house and walls. Plan to make a subroutine for player sprinting, so that they will have to pause for a few seconds between sprints

---
+++ HaloweenJam2019/Assets/Scripts/EnemyFollowing.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+    [RequireComponent(typeof(NavMeshAgent))]
+public class EnemyFollowing : MonoBehaviour
+{
+    GameObject target;
+    Vector3 destination;
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        target = GameObject.FindWithTag("Player");
+        agent = GetComponent<NavMeshAgent>();
+        destination = agent.destination;
+    }
+
+    void Update()
+    {
+        if (Vector3.Distance(destination, target.transform.position) > 1.0f)
+        {
+            destination = target.transform.position;
+            agent.destination = destination;
+        }
+    }
+}
