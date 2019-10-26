1b1e9160c67fe983c9b61e24fead050f898b1c7a
JackHill-SJ <hilljack1856@gmail.com>
2019-10-26 16:29:18 -0400

    LightKillsZombie

---
+++ HaloweenJam2019/Assets/Scripts/TurnOnLight.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOnLight : MonoBehaviour
+{
+    public GameObject Light;
+    bool InteractTrigger;
+
+    private void Start()
+    {
+        gameObject.transform.GetChild(0).gameObject.SetActive(false);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            InteractTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            InteractTrigger = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && (InteractTrigger == true))
+        {
+            gameObject.transform.GetChild(0).gameObject.SetActive(true);
+        }
+    }
+}
