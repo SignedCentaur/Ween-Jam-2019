b261bfc61b183173b608449fa6dd744a350e5f9b
Mint16384 <mint16384@gmail.com>
2019-10-26 16:08:50 -0400

    Gun animates and shoots

---
+++ HaloweenJam2019/Assets/Scripts/BulletStuff.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletStuff : MonoBehaviour
+{
+    public float bulletDamage;
+    public float bulletSpeed = 1.05f;
+
+    void Start()
+    {
+        GetComponent<Rigidbody>().velocity = transform.up * bulletSpeed;
+        Destroy(gameObject, .5f);
+    }
+    void Update()
+    {
+        
+    }
+}
---
+++ HaloweenJam2019/Assets/Scripts/GunUse.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunUse : MonoBehaviour
+{
+    [Header("GameObjects")]
+    public GameObject bullets;
+    public GameObject player;
+    public Transform gunEnd;
+    public Animator childAnimator;
+    [Header("Math Stuff")]
+    public int gunDamage;
+    public float fireRate;
+    public int bulletCount;
+    public float bulletSpread;
+    [Header("Bools")]
+    public bool isShooting = false;
+    public bool isReloading = false;
+
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            StartCoroutine("Shoot");
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            StartCoroutine("Reload");
+        }
+    }
+    IEnumerator Shoot()
+    {
+        if (!isShooting && !isReloading)
+        {
+            isShooting = true;
+
+            for (int i = 0; i < bulletCount; i += 1)
+            {
+                GameObject temp = Instantiate(bullets, gunEnd.transform.position, gunEnd.transform.rotation * Quaternion.Euler(90 + Random.Range(-bulletSpread / 2, bulletSpread / 2), 0, Random.Range(-bulletSpread / 2, bulletSpread / 2)));
+            }
+            childAnimator.SetTrigger("Shoot");
+
+            yield return new WaitForSeconds(fireRate);
+            isShooting = false;
+        }
+    }
+    IEnumerator Reload()
+    {
+        if (!isReloading && !isShooting)
+        {
+            isReloading = true;
+
+            childAnimator.SetTrigger("Reload");
+
+            yield return new WaitForSeconds(2.3f);
+            isReloading = false;
+        }
+    }
+}
